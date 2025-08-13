d9201025569dc50c458680eb5fb0178a61b0a7b5
Michael Bestas <mkbestas@lineageos.org>
2025-08-13 02:22:25 +0000

    Update fingerprint/build description from BP2A.250805.005
    Change-Id: I5fc205cf4ba3d45be65405bcc4847aec60cd5ab0

--- yaap_panther.mk
+++ yaap_panther.mk
@@ -31,8 +31,8 @@ TARGET_SUPPORTS_64_BIT_APPS := true
 TARGET_ENABLE_BLUR := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="panther-user 16 BP2A.250705.008 13578956 release-keys" \
-    BuildFingerprint=google/panther/panther:16/BP2A.250705.008/13578956:user/release-keys \
+    BuildDesc="panther-user 16 BP2A.250805.005 13691446 release-keys" \
+    BuildFingerprint=google/panther/panther:16/BP2A.250805.005/13691446:user/release-keys \
     DeviceProduct=$(DEVICE_CODENAME)
 
 $(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
